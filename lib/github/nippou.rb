654d622df3ffd4b7121a512bf07a1069514e0849
Takashi Masuda <masutaka.net@gmail.com>
2014-12-07 16:56:58 +0900

    Add github-nippou.user and github-nippou.token

--- lib/github/nippou.rb
+++ lib/github/nippou.rb
@@ -4,8 +4,9 @@ require 'octokit'
 module Github
   module Nippou
     def self.list
-      client = Octokit::Client.new(login: 'kitak', access_token: 'githubの/settings/applicationsで発行したトークン')
-      events = client.user_events('kitak')
+      user = self.user
+      client = Octokit::Client.new(login: user, access_token: self.access_token)
+      events = client.user_events(user)
 
       url_to_detail = {}
 
@@ -27,6 +28,46 @@ module Github
         detail[:comments].reverse.each do |comment|
           puts "  * #{comment}"
         end
+      end
+    end
+
+    private
+
+    def self.user
+      case
+      when ENV['GITHUB_NIPPOU_USER']
+        ENV['GITHUB_NIPPOU_USER']
+      when !`git config github-nippou.user`.chomp.empty?
+        `git config github-nippou.user`.chomp
+      else
+        puts <<MESSAGE
+** User required.
+
+Please set github-nippou.user to your .gitconfig.
+    $ git config --global github-nippou.user [Your GitHub account]
+MESSAGE
+        exit!
+      end
+    end
+
+    def self.access_token
+      case
+      when ENV['GITHUB_NIPPOU_ACCESS_TOKEN']
+        ENV['GITHUB_NIPPOU_ACCESS_TOKEN']
+      when !`git config github-nippou.token`.chomp.empty?
+        `git config github-nippou.token`.chomp
+      else
+        puts <<MESSAGE
+** Authorization required.
+
+Please set github-nippou.token to your .gitconfig.
+    $ git config --global github-nippou.token [Your GitHub access token]
+
+To get new token, visit
+https://github.com/settings/tokens/new
+
+MESSAGE
+        exit!
       end
     end
   end
