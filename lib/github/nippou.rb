2e747c5f59e1ee445b8aa7b4fa4046559f7b188d
Takashi Masuda <masutaka.net@gmail.com>
2016-03-21 19:52:27 +0900

    Refactor class method definition

--- lib/github/nippou.rb
+++ lib/github/nippou.rb
@@ -3,61 +3,61 @@ require 'octokit'
 
 module Github
   module Nippou
-    def self.list
-      user = self.user
-      client = Octokit::Client.new(login: user, access_token: self.access_token)
-      events = client.user_events(user)
+    class << self
+      def list
+        client = Octokit::Client.new(login: user, access_token: access_token)
+        events = client.user_events(user)
 
-      url_to_detail = {}
+        url_to_detail = {}
 
-      events.each do |_|
-        break unless _.created_at.getlocal.to_date == Time.now.to_date
-        case _.type
-        when 'IssuesEvent', 'IssueCommentEvent'
-          title = _.payload.issue.title.gsub('`', '\\\`')
-          merged = client.pull_merged?(_.repo.name, _.payload.issue.number)
-          url_to_detail[_.payload.issue.html_url] ||= {title: title, repo_basename: _.repo.name, username: _.payload.issue.user.login, merged: merged}
-        when 'PullRequestEvent', 'PullRequestReviewCommentEvent'
-          title = _.payload.pull_request.title.gsub('`', '\\\`')
-          merged = client.pull_merged?(_.repo.name, _.payload.pull_request.number)
-          url_to_detail[_.payload.pull_request.html_url] ||= {title: title, repo_basename: _.repo.name, username: _.payload.pull_request.user.login, merged: merged}
+        events.each do |_|
+          break unless _.created_at.getlocal.to_date == Time.now.to_date
+          case _.type
+          when 'IssuesEvent', 'IssueCommentEvent'
+            title = _.payload.issue.title.gsub('`', '\\\`')
+            merged = client.pull_merged?(_.repo.name, _.payload.issue.number)
+            url_to_detail[_.payload.issue.html_url] ||= {title: title, repo_basename: _.repo.name, username: _.payload.issue.user.login, merged: merged}
+          when 'PullRequestEvent', 'PullRequestReviewCommentEvent'
+            title = _.payload.pull_request.title.gsub('`', '\\\`')
+            merged = client.pull_merged?(_.repo.name, _.payload.pull_request.number)
+            url_to_detail[_.payload.pull_request.html_url] ||= {title: title, repo_basename: _.repo.name, username: _.payload.pull_request.user.login, merged: merged}
+          end
+        end
+
+        url_to_detail.each do |url, detail|
+          line = "* [#{detail[:title]} - #{detail[:repo_basename]}](#{url}) by #{detail[:username]}"
+          line << ' **merged!**' if detail[:merged]
+          puts line
         end
       end
 
-      url_to_detail.each do |url, detail|
-        line = "* [#{detail[:title]} - #{detail[:repo_basename]}](#{url}) by #{detail[:username]}"
-        line << ' **merged!**' if detail[:merged]
-        puts line
-      end
-    end
+      private
 
-    private
-
-    def self.user
-      case
-      when ENV['GITHUB_NIPPOU_USER']
-        ENV['GITHUB_NIPPOU_USER']
-      when !`git config github-nippou.user`.chomp.empty?
-        `git config github-nippou.user`.chomp
-      else
-        puts <<MESSAGE
+      def user
+        case
+        when ENV['GITHUB_NIPPOU_USER']
+          ENV['GITHUB_NIPPOU_USER']
+        when !`git config github-nippou.user`.chomp.empty?
+          `git config github-nippou.user`.chomp
+        else
+          puts <<MESSAGE
 ** User required.
 
 Please set github-nippou.user to your .gitconfig.
     $ git config --global github-nippou.user [Your GitHub account]
 MESSAGE
-        exit!
+          exit!
+        end
       end
-    end
 
-    def self.access_token
-      case
-      when ENV['GITHUB_NIPPOU_ACCESS_TOKEN']
-        ENV['GITHUB_NIPPOU_ACCESS_TOKEN']
-      when !`git config github-nippou.token`.chomp.empty?
-        `git config github-nippou.token`.chomp
-      else
-        puts <<MESSAGE
+      def access_token
+        case
+        when ENV['GITHUB_NIPPOU_ACCESS_TOKEN']
+          ENV['GITHUB_NIPPOU_ACCESS_TOKEN']
+        when !`git config github-nippou.token`.chomp.empty?
+          `git config github-nippou.token`.chomp
+        else
+          puts <<MESSAGE
 ** Authorization required.
 
 Please set github-nippou.token to your .gitconfig.
@@ -67,7 +67,8 @@ To get new token, visit
 https://github.com/settings/tokens/new
 
 MESSAGE
-        exit!
+          exit!
+        end
       end
     end
   end
