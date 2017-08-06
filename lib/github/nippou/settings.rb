2f6a54154637e9b7a19808c773d5505eaf1da3f2
Takashi Masuda <masutaka.net@gmail.com>
2017-08-06 17:55:44 +0900

    Refactor Github::Nippou::Settings#data

--- lib/github/nippou/settings.rb
+++ lib/github/nippou/settings.rb
@@ -9,34 +9,34 @@ module Github
       #
       # return [Hash]
       def data
-        return @data if @data.present?
+        @data ||=
+          begin
+            yaml_data =
+              case
+              when ENV['GITHUB_NIPPOU_SETTINGS']
+                ENV['GITHUB_NIPPOU_SETTINGS'].chomp
+              when !`git config github-nippou.settings`.chomp.empty?
+                `git config github-nippou.settings`.chomp
+              when !`git config github-nippou.settings-gist-id`.chomp.empty?
+                gist_id = `git config github-nippou.settings-gist-id`.chomp
+                gist = client.gist(gist_id)
+                gist[:files][:'settings.yml'][:content]
+              end
 
-        yaml_data =
-          case
-          when ENV['GITHUB_NIPPOU_SETTINGS']
-            ENV['GITHUB_NIPPOU_SETTINGS'].chomp
-          when !`git config github-nippou.settings`.chomp.empty?
-            `git config github-nippou.settings`.chomp
-          when !`git config github-nippou.settings-gist-id`.chomp.empty?
-            gist_id = `git config github-nippou.settings-gist-id`.chomp
-            gist = client.gist(gist_id)
-            gist[:files][:'settings.yml'][:content]
+            if yaml_data
+              YAML.load(yaml_data).deep_symbolize_keys
+            else
+              YAML.load_file(File.expand_path('../../../config/settings.yml', __dir__)).deep_symbolize_keys
+            end
+          rescue Psych::SyntaxError => e
+            puts <<~MESSAGE
+              ** YAML syntax error.
+
+              #{e.message}
+              #{yaml_data}
+            MESSAGE
+            exit
           end
-
-        @data =
-          if yaml_data
-            YAML.load(yaml_data).deep_symbolize_keys
-          else
-            YAML.load_file(File.expand_path('../../../config/settings.yml', __dir__)).deep_symbolize_keys
-          end
-      rescue Psych::SyntaxError => e
-        puts <<~MESSAGE
-          ** YAML syntax error.
-
-          #{e.message}
-          #{yaml_data}
-        MESSAGE
-        exit
       end
 
       private
