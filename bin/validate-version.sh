42b0e4d94bf209bf7472af1d555fea78c7d81897
Takashi Masuda <masutaka.net@gmail.com>
2017-08-07 21:47:18 +0900

    Refactor

--- bin/validate-version.sh
+++ bin/validate-version.sh
@@ -3,7 +3,15 @@
 a=lib/github/nippou/version.rb
 b=Dockerfile
 
-if [ $(fgrep VERSION $a | awk '{print $3}' | tr -d "'") != $(fgrep 'VERSION=' $b | awk -F = '{print $2}') ]; then
+version_of_file_a() {
+  fgrep VERSION $1 | awk '{print $3}' | tr -d "'"
+}
+
+version_of_file_b() {
+  fgrep 'VERSION=' $1 | awk -F = '{print $2}'
+}
+
+if [ $(version_of_file_a $a) != $(version_of_file_b $b) ]; then
   echo "The versions of $a and $b are different." >&2
   exit 1
 fi
