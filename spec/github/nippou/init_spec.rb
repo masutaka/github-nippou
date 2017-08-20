c6cf779bdc1d39503c45e6ec3c96ab1dc4a9c1e0
Takashi Masuda <masutaka.net@gmail.com>
2017-08-20 13:37:54 +0900

    Fix Layout/BlockEndNewline, Layout/MultilineBlockLayout and Style/BlockDelimiters

--- spec/github/nippou/init_spec.rb
+++ spec/github/nippou/init_spec.rb
@@ -2,8 +2,10 @@
 
 describe Github::Nippou::Init do
   let(:client) { Octokit::Client.new(login: 'taro', access_token: '1234abcd') }
-  let(:settings) { double(:settings, user: nil, access_token: nil, gist_id: nil, thread_num: nil,
-                          client: nil, create_gist: nil, url: nil, default_url: nil, format: nil, dictionary: nil) }
+  let(:settings) do
+    double(:settings, user: nil, access_token: nil, gist_id: nil, thread_num: nil,
+           client: nil, create_gist: nil, url: nil, default_url: nil, format: nil, dictionary: nil)
+  end
   let(:init) { described_class.new(settings: settings) }
 
   it_behaves_like 'a settings interface'
