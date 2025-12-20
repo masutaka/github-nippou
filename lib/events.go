4db0795ecc7fa4e9772f6dd43d2f26dd0dc17eed
Takashi Masuda <masutaka.net@gmail.com>
2025-12-20 15:00:49 +0900

    Replace github.com/google/go-github/v69/ to github.com/google/go-github/v80/

--- lib/events.go
+++ lib/events.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/google/go-github/v69/github"
+	"github.com/google/go-github/v80/github"
 )
 
 // Events represents a structure for fetching user-related events from the GitHub API.
